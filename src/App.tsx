ff35e5bd5877766b1b6bf28e2a1c5986dd61e2d3
Damir <adidukik@gmail.com>
2024-12-16 01:35:09 +0900

    remove bg

--- src/App.tsx
+++ src/App.tsx
@@ -301,7 +301,7 @@ export default function App() {
         />
       </div>
 
-      <div className='flex-1 overflow-y-auto bg-gray-50 p-4'>
+      <div className='flex-1 overflow-y-auto p-4 pb-48'>
         <div className='flex flex-col space-y-2 max-w-3xl mx-auto'>
           {messages.map((msg, idx) => renderMessage(msg, idx))}
 
